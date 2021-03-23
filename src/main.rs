67bf4f93118334f59161f22bed53d41be14bfa46
Jonathon Jongsma <jjongsma@redhat.com>
2021-03-23 11:50:52 -0500

    Implement 'mdevctl list'
    Add an implementation of the list command. This handles both the
    standard text-based output and the json-formatted output, and aims to be
    identical to the existing mdevctl command and output. There was no
    attempt to optimize for performance other than writing it in a compiled
    language, but the performance is already miles better. Here are the
    results with 20 devices defined on my laptop:
    
    CURRENT SHELL SCRIPT IMPLEMENTATION
    -----------------------------------
        $ time mdevctl list --defined --dumpjson
        [
          {
            "0000:00:02.0": [
              {
                "88a6b868-46bd-4015-8e5b-26107f82da38": {
                  "mdev_type": "i915-GVTg_V5_8",
                  "start": "manual"
                }
              },
              [[[ ... SNIPPED 19 additional devices ... ]]]
            ]
          }
        ]
    
        real    0m6.713s
        user    0m6.441s
        sys     0m0.266s
    
    NEW RUST IMPLEMENTATION
    -----------------------
        $ time cargo run list --defined --dumpjson
        Finished dev [unoptimized + debuginfo] target(s) in 0.02s
        Running `target/debug/mdevctl list --defined --dumpjson`
        [
          {
            "0000:00:02.0": [
              {
                "88a6b868-46bd-4015-8e5b-26107f82da38": {
                  "mdev_type": "i915-GVTg_V5_8",
                  "start": "manual",
                  "attrs": []
                }
              },
              [[[ ... SNIPPED 19 additional devices ... ]]]
            ]
          }
        ]
    
        real    0m0.040s
        user    0m0.029s
        sys     0m0.010s
    
    Signed-off-by: Jonathon Jongsma <jjongsma@redhat.com>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use anyhow::{anyhow, Result};
 use log::{debug, warn};
+use serde_json;
+use std::collections::BTreeMap;
 use std::fs;
+use std::io::Read;
 use std::path::{Path, PathBuf};
 use structopt::StructOpt;
 use uuid::Uuid;
@@ -103,6 +106,11 @@ fn canonical_basename<P: AsRef<Path>>(path: P) -> Result<String> {
     }
 }
 
+enum FormatType {
+    Active,
+    Defined,
+}
+
 #[derive(Debug)]
 struct MdevInfo {
     uuid: Uuid,
@@ -112,6 +120,7 @@ struct MdevInfo {
     path: PathBuf,
     parent: String,
     mdev_type: String,
+    attrs: BTreeMap<String, String>,
 }
 
 impl MdevInfo {
@@ -124,6 +133,7 @@ impl MdevInfo {
             path: PathBuf::new(),
             parent: String::new(),
             mdev_type: String::new(),
+            attrs: BTreeMap::new(),
         }
     }
 
@@ -170,6 +180,147 @@ impl MdevInfo {
         debug!("loaded device {:?}", self);
         Ok(())
     }
+
+    pub fn load_from_json(&mut self, parent: String, json: &serde_json::Value) -> Result<()> {
+        debug!(
+            "Loading device '{:?}' from json (parent: {})",
+            self.uuid, parent
+        );
+        self.defined = true;
+        if !self.parent.is_empty() && self.parent != parent {
+            warn!(
+                "Overwriting parent for mdev {:?}: {} => {}",
+                self.uuid, self.parent, parent
+            );
+        }
+        self.parent = parent;
+        if json["mdev_type"].is_null() || json["start"].is_null() {
+            return Err(anyhow!("invalid json"));
+        }
+        let mdev_type = json["mdev_type"].as_str().unwrap().to_string();
+        if !self.mdev_type.is_empty() && self.mdev_type != mdev_type {
+            warn!(
+                "Overwriting mdev type for mdev {:?}: {} => {}",
+                self.uuid, self.mdev_type, mdev_type
+            );
+        }
+        self.mdev_type = mdev_type;
+        let startval = json["start"].as_str();
+        self.autostart = match startval {
+            Some("auto") => true,
+            _ => false,
+        };
+
+        match json["attrs"].as_array() {
+            Some(_) => {
+                let attrarray = json["attrs"].as_array().unwrap();
+                if !attrarray.is_empty() {
+                    for attr in json["attrs"].as_array().unwrap() {
+                        let attrobj = attr.as_object().unwrap();
+                        for (key, val) in attrobj.iter() {
+                            let valstr = val.as_str().unwrap();
+                            self.attrs.insert(key.to_string(), valstr.to_string());
+                        }
+                    }
+                }
+            }
+            _ => (),
+        };
+        debug!("loaded device {:?}", self);
+
+        Ok(())
+    }
+
+    pub fn to_text(&self, fmt: &FormatType, verbose: bool) -> Result<String> {
+        match fmt {
+            FormatType::Defined => {
+                if !self.defined {
+                    return Err(anyhow!("Device is not defined"));
+                }
+            }
+            FormatType::Active => {
+                if !self.active {
+                    return Err(anyhow!("Device is not active"));
+                }
+            }
+        }
+
+        let mut output = self.uuid.to_hyphenated().to_string();
+        output.push(' ');
+        output.push_str(&self.parent);
+        output.push(' ');
+        output.push_str(&self.mdev_type);
+        output.push(' ');
+        output.push_str(match self.autostart {
+            true => "auto",
+            false => "manual",
+        });
+
+        match fmt {
+            FormatType::Defined => {
+                if self.active {
+                    output.push_str(" (active)");
+                }
+            }
+            FormatType::Active => {
+                if self.defined {
+                    output.push_str(" (defined)");
+                }
+            }
+        }
+
+        output.push('\n');
+        if verbose && self.attrs.len() > 0 {
+            let mut i = 0;
+            output.push_str("  Attrs:\n");
+            for (key, value) in &self.attrs {
+                let txtattr = format!("    @{{{}}}: {{\"{}\":\"{}\"}}\n", i, key, value);
+                output.push_str(&txtattr);
+                i += 1;
+            }
+        }
+        Ok(output)
+    }
+
+    pub fn to_json(&self) -> Result<serde_json::Value> {
+        let autostart = match self.autostart {
+            true => "auto",
+            false => "manual",
+        };
+        let mut jsonattrs = serde_json::json!([]);
+        if self.attrs.len() > 0 {
+            for (key, value) in &self.attrs {
+                let attr = serde_json::json!({ key: value });
+                jsonattrs.as_array_mut().unwrap().push(attr);
+            }
+        }
+        let jsonval: serde_json::Value = serde_json::json!({
+            self.uuid.to_hyphenated().to_string(): {
+                "mdev_type": self.mdev_type,
+                "start": autostart,
+                "attrs": jsonattrs
+            }
+        });
+
+        Ok(jsonval)
+    }
+}
+
+fn format_json(devices: BTreeMap<String, Vec<MdevInfo>>) -> Result<String> {
+    let mut parents = serde_json::map::Map::new();
+    for (parentname, children) in devices {
+        let mut childrenarray = Vec::new();
+        for child in children {
+            childrenarray.push(child.to_json()?);
+        }
+        parents.insert(parentname, childrenarray.into());
+    }
+    // don't serialize an empty object if there are no devices
+    let jsonval = match parents.len() {
+        0 => serde_json::json!([]),
+        _ => serde_json::json!([parents]),
+    };
+    serde_json::to_string_pretty(&jsonval).map_err(|_e| anyhow!("Unable to serialize json"))
 }
 
 fn define_command(
@@ -221,13 +372,124 @@ fn stop_command(uuid: Uuid) -> Result<()> {
 }
 
 fn list_command(
-    _defined: bool,
-    _dumpjson: bool,
-    _verbose: bool,
-    _uuid: Option<Uuid>,
-    _parent: Option<String>,
+    defined: bool,
+    dumpjson: bool,
+    verbose: bool,
+    uuid: Option<Uuid>,
+    parent: Option<String>,
 ) -> Result<()> {
-    return Err(anyhow!("Not implemented"));
+    let mut devices: BTreeMap<String, Vec<MdevInfo>> = BTreeMap::new();
+    if defined {
+        debug!("Looking up defined mdevs");
+        for parentpath in PathBuf::from(PERSIST_BASE).read_dir()? {
+            let parentpath = parentpath?;
+            let parentname = parentpath.file_name();
+            let parentname = parentname.to_str().unwrap();
+            if (parent.is_some() && parent.as_ref().unwrap() != parentname)
+                || !parentpath.metadata()?.is_dir()
+            {
+                debug!("Ignoring child devices for parent {}", parentname);
+                continue;
+            }
+
+            let mut childdevices = Vec::new();
+
+            for child in parentpath.path().read_dir()? {
+                let child = child?;
+                if !child.metadata()?.is_file() {
+                    continue;
+                }
+
+                let path = child.path();
+                let basename = path.file_name().unwrap().to_str().unwrap();
+                let u = Uuid::parse_str(basename).unwrap();
+                debug!("found mdev {:?}", u);
+                if uuid.is_some() && uuid.as_ref().unwrap() != &u {
+                    debug!(
+                        "Ignoring device {} because it doesn't match uuid {}",
+                        u,
+                        uuid.unwrap()
+                    );
+                    continue;
+                }
+
+                let mut f = fs::File::open(path)?;
+                let mut contents = String::new();
+                f.read_to_string(&mut contents)?;
+                let val: serde_json::Value = serde_json::from_str(&contents)?;
+                let mut dev = MdevInfo::new(u);
+                dev.load_from_json(parentname.to_string(), &val)?;
+                dev.load_from_sysfs()?;
+
+                childdevices.push(dev);
+            }
+            devices.insert(parentname.to_string(), childdevices);
+        }
+    } else {
+        debug!("Looking up active mdevs");
+        for dev in PathBuf::from(MDEV_BASE).read_dir()? {
+            let dev = dev?;
+            let fname = dev.file_name();
+            let basename = fname.to_str().unwrap();
+            debug!("found defined mdev {}", basename);
+            let u = Uuid::parse_str(basename).unwrap();
+
+            if uuid.is_some() && u != uuid.unwrap() {
+                debug!(
+                    "Ignoring device {} because it doesn't match uuid {}",
+                    u,
+                    uuid.unwrap()
+                );
+                continue;
+            }
+
+            let mut info = MdevInfo::new(u);
+            match info.load_from_sysfs() {
+                Ok(_) => {
+                    if parent.is_some() {
+                        match parent.as_ref() {
+                            Some(p) => {
+                                if p.as_ref() != info.parent {
+                                    debug!(
+                                        "Ignoring device {} because it doesn't match parent {}",
+                                        info.uuid, p
+                                    );
+                                    continue;
+                                }
+                            }
+                            None => (),
+                        }
+                    }
+
+                    if !devices.contains_key(&info.parent) {
+                        devices.insert(info.parent.clone(), Vec::new());
+                    };
+
+                    devices.get_mut(&info.parent).unwrap().push(info);
+                }
+                _ => (),
+            };
+        }
+    }
+
+    if dumpjson {
+        let output = format_json(devices)?;
+        println!("{}", output);
+    } else {
+        let mut output = String::new();
+        for (_parent, children) in devices {
+            let ft = match defined {
+                true => FormatType::Defined,
+                false => FormatType::Active,
+            };
+            for dev in children {
+                output.push_str(&dev.to_text(&ft, verbose)?);
+            }
+        }
+        println!("{}", output);
+    }
+
+    Ok(())
 }
 
 fn types_command(_parent: Option<String>, _dumpjson: bool) -> Result<()> {
