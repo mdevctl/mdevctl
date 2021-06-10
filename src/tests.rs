97db6943a02925d5d86309936f01296e68c254b3
Jonathon Jongsma <jjongsma@redhat.com>
2021-06-10 13:51:39 -0500

    Add tests for the 'list' command
    Signed-off-by: Jonathon Jongsma <jjongsma@redhat.com>

--- src/tests.rs
+++ src/tests.rs
@@ -846,3 +846,212 @@ fn test_invalid_files() {
     let result = crate::list_command(&test, true, false, false, None, None);
     assert!(result.is_ok());
 }
+
+fn test_list_helper(
+    test: &TestEnvironment,
+    subtest: &str,
+    expect: Expect,
+    defined: bool,
+    verbose: bool,
+    uuid: Option<String>,
+    parent: Option<String>,
+) {
+    use crate::list_command_helper;
+    let uuid = uuid.map(|s| Uuid::parse_str(s.as_ref()).unwrap());
+
+    let expectedtext = test.datapath.join(format!("{}.text", subtest));
+    let res = list_command_helper(test, defined, false, verbose, uuid, parent.clone());
+    if expect == Expect::Fail {
+        res.expect_err("expected list command to fail");
+        return;
+    }
+
+    let output = res.expect("list command failed unexpectedly");
+    compare_to_file(&expectedtext, &output);
+
+    let expectedjson = test.datapath.join(format!("{}.json", subtest));
+    let res = list_command_helper(test, defined, true, verbose, uuid, parent.clone());
+    if expect == Expect::Fail {
+        res.expect_err("expected list command to fail");
+        return;
+    }
+
+    let output = res.expect("list command failed unexpectedly");
+    compare_to_file(&expectedjson, &output);
+}
+
+#[test]
+fn test_list() {
+    init();
+
+    let test = TestEnvironment::new("list", "default");
+
+    const UUID: &[&str] = &[
+        "976d8cc2-4bfc-43b9-b9f9-f4af2de91ab9",
+        "59e8b599-afdd-4766-a59e-415ef4f5a492",
+        "4a0a190f-dcf3-4def-9342-c48768f0c940",
+        "9f579710-6ffc-4201-987a-4ffa0fb1f3a5",
+        "3eee6cd9-35ad-43bd-9be1-14ee2b7389c9",
+    ];
+    const PARENT: &[&str] = &["0000:00:02.0", "0000:00:03.0"];
+    const MDEV_TYPE: &[&str] = &["arbitrary_type1", "arbitrary_type2"];
+    // first test with an empty environment -- nothing defined, nothing active
+    test_list_helper(&test, "active-none", Expect::Pass, false, false, None, None);
+    test_list_helper(&test, "defined-none", Expect::Pass, true, false, None, None);
+
+    // now setup test environment with some active devices and some defined devices. Include
+    // multiple parents, multiple types, some parents with multiple devices, some with same UUID on
+    // different parents, etc
+    test.populate_active_device(UUID[0], PARENT[0], MDEV_TYPE[0]);
+    test.populate_active_device(UUID[1], PARENT[1], MDEV_TYPE[1]);
+    test.populate_defined_device(UUID[2], PARENT[0], "device2.json");
+    test.populate_defined_device(UUID[3], PARENT[1], "device1.json");
+    test.populate_defined_device(UUID[3], PARENT[0], "device1.json");
+
+    test_list_helper(&test, "active", Expect::Pass, false, false, None, None);
+    test_list_helper(
+        &test,
+        "active-verbose",
+        Expect::Pass,
+        false,
+        true,
+        None,
+        None,
+    );
+    test_list_helper(
+        &test,
+        "active-parent",
+        Expect::Pass,
+        false,
+        false,
+        None,
+        Some(PARENT[0].to_string()),
+    );
+    test_list_helper(
+        &test,
+        "active-parent-verbose",
+        Expect::Pass,
+        false,
+        true,
+        None,
+        Some(PARENT[0].to_string()),
+    );
+    test_list_helper(
+        &test,
+        "active-uuid",
+        Expect::Pass,
+        false,
+        false,
+        Some(UUID[0].to_string()),
+        None,
+    );
+    test_list_helper(
+        &test,
+        "active-uuid-verbose",
+        Expect::Pass,
+        false,
+        true,
+        Some(UUID[0].to_string()),
+        None,
+    );
+    test_list_helper(
+        &test,
+        "active-uuid-parent",
+        Expect::Pass,
+        false,
+        false,
+        Some(UUID[0].to_string()),
+        Some(PARENT[0].to_string()),
+    );
+    test_list_helper(
+        &test,
+        "active-uuid-parent-verbose",
+        Expect::Pass,
+        false,
+        true,
+        Some(UUID[0].to_string()),
+        Some(PARENT[0].to_string()),
+    );
+    test_list_helper(&test, "defined", Expect::Pass, true, false, None, None);
+    test_list_helper(
+        &test,
+        "defined-verbose",
+        Expect::Pass,
+        true,
+        true,
+        None,
+        None,
+    );
+    test_list_helper(
+        &test,
+        "defined-parent",
+        Expect::Pass,
+        true,
+        false,
+        None,
+        Some(PARENT[0].to_string()),
+    );
+    test_list_helper(
+        &test,
+        "defined-parent-verbose",
+        Expect::Pass,
+        true,
+        true,
+        None,
+        Some(PARENT[0].to_string()),
+    );
+    test_list_helper(
+        &test,
+        "defined-uuid",
+        Expect::Pass,
+        true,
+        false,
+        Some(UUID[3].to_string()),
+        None,
+    );
+    test_list_helper(
+        &test,
+        "defined-uuid-verbose",
+        Expect::Pass,
+        true,
+        true,
+        Some(UUID[3].to_string()),
+        None,
+    );
+    test_list_helper(
+        &test,
+        "defined-uuid-parent",
+        Expect::Pass,
+        true,
+        false,
+        Some(UUID[3].to_string()),
+        Some(PARENT[0].to_string()),
+    );
+    test_list_helper(
+        &test,
+        "defined-uuid-parent-verbose",
+        Expect::Pass,
+        true,
+        true,
+        Some(UUID[3].to_string()),
+        Some(PARENT[0].to_string()),
+    );
+    test_list_helper(
+        &test,
+        "no-match-uuid",
+        Expect::Pass,
+        true,
+        true,
+        Some("466983a3-1240-4543-8d02-01c29a08fb0c".to_string()),
+        None,
+    );
+    test_list_helper(
+        &test,
+        "no-match-parent",
+        Expect::Pass,
+        true,
+        true,
+        None,
+        Some("nonexistent".to_string()),
+    );
+}
