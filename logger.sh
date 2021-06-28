6d58d7e6a5f46dd9794e731a99cd182845b121a8
Collin Walling <walling@linux.ibm.com>
2021-06-28 13:54:27 -0400

    Sample notifer script
    As an example of the call-out mechanism, a "logger" script
    can be installed to /etc/mdevctl.d/scripts.d/notifiers/
    to assist with debugging the I/O for call-out scripts. All
    messages are printed to the system log.
    
    Signed-off-by: Collin Walling <walling@linux.ibm.com>

---
+++ logger.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+# A basic utility script used for debugging notification call-out events
+# in mdevctl. Output can be observed via system logs.
+#
+# Place this script in /etc/mdevctl.d/scripts.d/notifiers/ and run
+# a supported command, then check the system logs for output.
+
+#stdin | -e event -a action -s state -u uuid -p parent
+shift
+event=$1
+shift 2
+action=$1
+shift 2
+state=$1
+shift 2
+uuid=$1
+shift 2
+parent=$1
+json=$(</dev/stdin)
+
+if [[ "$event" == "notify" ]]; then
+	echo "Event: $event, Action: $action, State: $state, UUID: $uuid, Parent: $parent, JSON:" | logger -t "mdevctl_logger"
+	echo "$json" | logger -t "mdevctl_logger"
+fi
