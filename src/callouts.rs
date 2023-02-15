ec80cb07002b255358d7b109f3ea8906549f663c
Boris Fiuczynski <fiuczy@linux.ibm.com>
2023-02-15 17:40:44 +0100

    callouts: add warning for unknown supported actions and events
    Adding a warning when a callout script supports versioning and reports
    actions or events that are unknown to the mdevctl code.
    This can be caused by a callout script supporting also newer versions of
    mdevctl which are supporting these or by a mistake in the callout
    script.
    
    Signed-off-by: Boris Fiuczynski <fiuczy@linux.ibm.com>

--- src/callouts.rs
+++ src/callouts.rs
@@ -380,6 +380,12 @@ impl CheckProcessOutput for CapabilitiesCheckProcessOutput {
         match CalloutScriptCache::parse_script_capabilities(&o) {
             Some(cv) => {
                 debug!(" Script supports versioning: {:?}", cv);
+                if cv.has_action(Action::Unknown) {
+                    warn!("Callout script {:?} provides unknown Action type", p);
+                }
+                if cv.has_event(Event::Unknown) {
+                    warn!("Callout script {:?} provides unknown Event type", p);
+                }
                 c.script = Some(CalloutScriptInfo::new(
                     p,
                     c.dev.parent().unwrap().to_string(),
