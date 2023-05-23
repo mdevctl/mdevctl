cebc8dc11f19f17b07c8543a7c84a24116f840aa
Boris Fiuczynski <fiuczy@linux.ibm.com>
2023-05-23 17:09:30 +0200

    Update src/callouts.rs
    This code has changed in the rebasing/refactoring.
    
    Co-authored-by: Jonathon Jongsma <jjongsma@redhat.com>

--- src/callouts.rs
+++ src/callouts.rs
@@ -369,11 +369,8 @@ impl CalloutScripts {
 
     fn parse_script_output(&self, output: Output) -> Result<CalloutVersion, CalloutError> {
         let stdout = String::from_utf8(output.stdout).unwrap();
-        let ce: CalloutExchange =
-            match serde_json::from_str(&stdout).map_err(CalloutError::InvalidJSON) {
-                Ok(ce) => ce,
-                Err(e) => return Err(e),
-            };
+        let ce: CalloutExchange = 
+            serde_json::from_str(&stdout).map_err(CalloutError::InvalidJSON)?;
         ce.supports.ok_or(CalloutError::NoSupportedVersion)
     }
 
