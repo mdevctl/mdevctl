mdevctl/mdevctl

commit f23dea4847246b36e629e60ed64c15ac9cb8c7f6

Remove use of faccess crate
Signed-off-by: Jonathon Jongsma <jjongsma@redhat.com>

src/main.rs
@@ -1,5 +1,4 @@
 use anyhow::{anyhow, ensure, Context, Result};
-use faccess::PathExt;
 use log::{debug, warn};
 use std::collections::BTreeMap;
 use std::convert::TryInto;
@@ -651,9 +650,8 @@ fn define_command_helper(
     let mut dev = MdevInfo::new(env, uuid);
 
     if let Some(jsonfile) = jsonfile {
-        if !jsonfile.readable() {
-            return Err(anyhow!("Unable to read file {:?}", jsonfile));
-        }
+        let _ = std::fs::File::open(&jsonfile)
+            .with_context(|| format!("Unable to read file {:?}", jsonfile));
 
         if mdev_type.is_some() {
             return Err(anyhow!(
@@ -793,8 +791,6 @@ fn write_attr(basepath: &Path, attr: &str, val: &str) -> Result<()> {
     let path = basepath.join(attr);
     if !path.exists() {
         return Err(anyhow!("Invalid attribute '{}'", val));
-    } else if !path.writable() {
-        return Err(anyhow!("Attribute '{}' cannot be set", val));
     }
     fs::write(path, val).with_context(|| format!("Failed to write {} to attribute {}", val, attr))
 }

Cargo.toml
@@ -17,5 +17,4 @@ log = "0.4"
 serde_json = {version = "1.0", features = ["preserve_order"]}
 structopt = "0.3.13"
 uuid = {version = "0.8.2", features = ["v4"]}
-faccess = "0.2.3"
 tempdir = "0.3"

commit 873e025fe07026564a42cb968535052580cfcb9c

main: fix file open error being suppressed
This was originally introduced by commit f23dea4 and than spread by
commit 863a082.

Signed-off-by: Boris Fiuczynski <fiuczy@linux.ibm.com>

src/main.rs
@@ -65,7 +65,7 @@ fn define_command_helper(
 
     if let Some(jsonfile) = jsonfile {
         let _ = std::fs::File::open(&jsonfile)
-            .with_context(|| format!("Unable to read file {:?}", jsonfile));
+            .with_context(|| format!("Unable to read file {:?}", jsonfile))?;
 
         if mdev_type.is_some() {
             return Err(anyhow!(
@@ -215,7 +215,7 @@ fn modify_command(
 
     if let Some(jsonfile) = jsonfile {
         let _ = std::fs::File::open(&jsonfile)
-            .with_context(|| format!("Unable to read file {:?}", jsonfile));
+            .with_context(|| format!("Unable to read file {:?}", jsonfile))?;
 
         let mut d = MDev::new(env, uuid);
         let filecontents = fs::read_to_string(&jsonfile)