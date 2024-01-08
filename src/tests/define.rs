1b76b6b550c96af5adee961edc3308b0fe0aba0f
Jonathon Jongsma <jjongsma@redhat.com>
2024-01-08 14:40:13 -0600

    split up tests
    The single tests.rs file was getting huge and hard to manage. Split the
    tests into a separate module with sub-modules for each category of test.
    
    Signed-off-by: Jonathon Jongsma <jjongsma@redhat.com>

---
+++ src/tests/define.rs
@@ -0,0 +1,497 @@
+use super::*;
+use std::{fs, path::PathBuf};
+use uuid::Uuid;
+
+fn test_define_command_callout<F>(
+    testname: &str,
+    expect: Expect,
+    uuid: Option<Uuid>,
+    parent: Option<String>,
+    mdev_type: Option<String>,
+    force: bool,
+    setupfn: F,
+) where
+    F: Fn(&TestEnvironment),
+{
+    let test = TestEnvironment::new("define-callouts", testname);
+    setupfn(&test);
+
+    use crate::define_command;
+    let res = define_command(&test, uuid, false, parent, mdev_type, None, force);
+
+    let _ = test.assert_result(res, expect, None);
+}
+
+fn test_define_helper<F>(
+    testname: &str,
+    expect: Expect,
+    uuid: Option<Uuid>,
+    auto: bool,
+    parent: Option<String>,
+    mdev_type: Option<String>,
+    jsonfile: Option<PathBuf>,
+    setupfn: F,
+) where
+    F: Fn(&TestEnvironment),
+{
+    use crate::define_command_helper;
+    let test = TestEnvironment::new("define", testname);
+
+    // load the jsonfile from the test path.
+    let jsonfile = match jsonfile {
+        Some(f) => Some(test.datapath.join(f)),
+        None => None,
+    };
+
+    setupfn(&test);
+
+    let res = define_command_helper(&test, uuid, auto, parent, mdev_type, jsonfile);
+    if let Ok(def) = test.assert_result(res, expect, None) {
+        let path = def.persist_path().unwrap();
+        assert!(!path.exists());
+        def.define().expect("Failed to define device");
+        assert!(path.exists());
+        assert!(def.is_defined());
+        let filecontents = fs::read_to_string(&path).unwrap();
+        test.compare_to_file(&format!("{}.expected", testname), &filecontents);
+    }
+}
+
+#[test]
+fn test_define() {
+    init();
+
+    const DEFAULT_UUID: &str = "976d8cc2-4bfc-43b9-b9f9-f4af2de91ab9";
+    const DEFAULT_PARENT: &str = "0000:00:03.0";
+    test_define_helper(
+        "no-uuid-no-type",
+        Expect::Fail(None),
+        None,
+        true,
+        Some(DEFAULT_PARENT.to_string()),
+        None,
+        None,
+        |_| {},
+    );
+    // if no uuid is specified, one will be auto-generated
+    test_define_helper(
+        "no-uuid",
+        Expect::Pass,
+        None,
+        true,
+        Some(DEFAULT_PARENT.to_string()),
+        Some("i915-GVTg_V5_4".to_string()),
+        None,
+        |_| {},
+    );
+    // specify autostart
+    test_define_helper(
+        "uuid-auto",
+        Expect::Pass,
+        Uuid::parse_str(DEFAULT_UUID).ok(),
+        true,
+        Some(DEFAULT_PARENT.to_string()),
+        Some("i915-GVTg_V5_4".to_string()),
+        None,
+        |_| {},
+    );
+    // specify manual start
+    test_define_helper(
+        "uuid-manual",
+        Expect::Pass,
+        Uuid::parse_str(DEFAULT_UUID).ok(),
+        false,
+        Some(DEFAULT_PARENT.to_string()),
+        Some("i915-GVTg_V5_4".to_string()),
+        None,
+        |_| {},
+    );
+    // invalid to specify an separate mdev_type if defining via jsonfile
+    test_define_helper(
+        "jsonfile-type",
+        Expect::Fail(None),
+        Uuid::parse_str(DEFAULT_UUID).ok(),
+        false,
+        Some(DEFAULT_PARENT.to_string()),
+        Some("i915-GVTg_V5_4".to_string()),
+        Some(PathBuf::from("defined.json")),
+        |_| {},
+    );
+    // specifying via jsonfile properly
+    test_define_helper(
+        "jsonfile",
+        Expect::Pass,
+        Uuid::parse_str(DEFAULT_UUID).ok(),
+        false,
+        Some(DEFAULT_PARENT.to_string()),
+        None,
+        Some(PathBuf::from("defined.json")),
+        |_| {},
+    );
+    // If uuid is already active, specifying mdev_type will result in an error
+    test_define_helper(
+        "uuid-running-no-parent",
+        Expect::Fail(None),
+        Uuid::parse_str(DEFAULT_UUID).ok(),
+        false,
+        None,
+        Some("i915-GVTg_V5_4".to_string()),
+        None,
+        |test| {
+            test.populate_active_device(DEFAULT_UUID, DEFAULT_PARENT, "i915-GVTg_V5_4");
+        },
+    );
+    // If uuid is already active, should use mdev_type from running mdev
+    test_define_helper(
+        "uuid-running-no-type",
+        Expect::Pass,
+        Uuid::parse_str(DEFAULT_UUID).ok(),
+        false,
+        Some(DEFAULT_PARENT.to_string()),
+        None,
+        None,
+        |test| {
+            test.populate_active_device(DEFAULT_UUID, DEFAULT_PARENT, "i915-GVTg_V5_4");
+        },
+    );
+    // ok to define a device with the same uuid as a running device even if they have different
+    // parent devices
+    test_define_helper(
+        "uuid-running-diff-parent",
+        Expect::Pass,
+        Uuid::parse_str(DEFAULT_UUID).ok(),
+        false,
+        Some(DEFAULT_PARENT.to_string()),
+        Some("i915-GVTg_V5_4".to_string()),
+        None,
+        |test| {
+            test.populate_active_device(DEFAULT_UUID, "0000:00:02.0", "i915-GVTg_V5_4");
+        },
+    );
+    // ok to define a device with the same uuid as a running device even if they have different
+    // mdev_types
+    test_define_helper(
+        "uuid-running-diff-type",
+        Expect::Pass,
+        Uuid::parse_str(DEFAULT_UUID).ok(),
+        false,
+        Some(DEFAULT_PARENT.to_string()),
+        Some("i915-GVTg_V5_4".to_string()),
+        None,
+        |test| {
+            test.populate_active_device(DEFAULT_UUID, DEFAULT_PARENT, "different_type");
+        },
+    );
+    // defining a device that is already defined should result in an error
+    test_define_helper(
+        "uuid-already-defined",
+        Expect::Fail(None),
+        Uuid::parse_str(DEFAULT_UUID).ok(),
+        false,
+        Some(DEFAULT_PARENT.to_string()),
+        Some("i915-GVTg_V5_4".to_string()),
+        None,
+        |test| {
+            test.populate_defined_device(DEFAULT_UUID, DEFAULT_PARENT, "defined.json");
+        },
+    );
+
+    // test define with callouts
+    test_define_command_callout(
+        "define-with-callout-all-pass",
+        Expect::Pass,
+        Uuid::parse_str(DEFAULT_UUID).ok(),
+        Some(DEFAULT_PARENT.to_string()),
+        Some("i915-GVTg_V5_4".to_string()),
+        false,
+        |test| {
+            test.populate_callout_script("rc0.sh");
+        },
+    );
+    test_define_command_callout(
+        "define-with-callout-all-fail",
+        Expect::Fail(None),
+        Uuid::parse_str(DEFAULT_UUID).ok(),
+        Some(DEFAULT_PARENT.to_string()),
+        Some("i915-GVTg_V5_4".to_string()),
+        false,
+        |test| {
+            test.populate_callout_script("rc1.sh");
+        },
+    );
+    // test define with get attributes
+    test_define_command_callout(
+        "define-with-callout-all-good-json",
+        Expect::Pass,
+        Uuid::parse_str(DEFAULT_UUID).ok(),
+        Some(DEFAULT_PARENT.to_string()),
+        Some("i915-GVTg_V5_4".to_string()),
+        false,
+        |test| {
+            test.populate_active_device(DEFAULT_UUID, DEFAULT_PARENT, "i915-GVTg_V5_4");
+            test.populate_callout_script("good-json.sh");
+        },
+    );
+    test_define_command_callout(
+        "define-with-callout-all-bad-json",
+        Expect::Fail(None),
+        Uuid::parse_str(DEFAULT_UUID).ok(),
+        Some(DEFAULT_PARENT.to_string()),
+        Some("i915-GVTg_V5_4".to_string()),
+        false,
+        |test| {
+            test.populate_active_device(DEFAULT_UUID, DEFAULT_PARENT, "i915-GVTg_V5_4");
+            test.populate_callout_script("bad-json.sh");
+        },
+    );
+    test_define_command_callout(
+        "define-with-callout-all-fail-force",
+        Expect::Pass,
+        Uuid::parse_str(DEFAULT_UUID).ok(),
+        Some(DEFAULT_PARENT.to_string()),
+        Some("i915-GVTg_V5_4".to_string()),
+        true,
+        |test| {
+            test.populate_callout_script("rc1.sh");
+        },
+    );
+
+    // test define with versioning callouts
+    // uuid=11111111-1111-0000-0000-000000000000 has a supported version
+    test_define_command_callout(
+        "define-with-version-callout-all-pass",
+        Expect::Pass,
+        Uuid::parse_str("11111111-1111-0000-0000-000000000000").ok(),
+        Some(DEFAULT_PARENT.to_string()),
+        Some("i915-GVTg_V5_4".to_string()),
+        false,
+        |test| {
+            test.populate_callout_script("ver-rc0.sh"); // versioning
+        },
+    );
+    test_define_command_callout(
+        "define-with-version-callout-all-fail",
+        Expect::Fail(None),
+        Uuid::parse_str("11111111-1111-0000-0000-000000000000").ok(),
+        Some(DEFAULT_PARENT.to_string()),
+        Some("i915-GVTg_V5_4".to_string()),
+        false,
+        |test| {
+            test.populate_callout_script("ver-rc1.sh"); // versioning error
+        },
+    );
+    test_define_command_callout(
+        "define-with-version-callout-multiple-with-version-pass",
+        Expect::Pass,
+        Uuid::parse_str("11111111-1111-0000-0000-000000000000").ok(),
+        Some(DEFAULT_PARENT.to_string()),
+        Some("i915-GVTg_V5_4".to_string()),
+        false,
+        |test| {
+            test.populate_callout_script("rc0.sh"); // no versioning
+            test.populate_callout_script("ver-rc0.sh"); // versioning
+        },
+    );
+    test_define_command_callout(
+        "define-with-version-callout-multiple-with-version-pass2",
+        Expect::Pass,
+        Uuid::parse_str("11111111-1111-0000-0000-000000000000").ok(),
+        Some(DEFAULT_PARENT.to_string()),
+        Some("i915-GVTg_V5_4".to_string()),
+        false,
+        |test| {
+            test.populate_callout_script("rc1.sh"); // no versioning error
+            test.populate_callout_script("ver-rc0.sh"); // versioning
+        },
+    );
+    test_define_command_callout(
+        "define-with-version-callout-multiple-with-version-fail",
+        Expect::Fail(None),
+        Uuid::parse_str("11111111-1111-0000-0000-000000000000").ok(),
+        Some(DEFAULT_PARENT.to_string()),
+        Some("i915-GVTg_V5_4".to_string()),
+        false,
+        |test| {
+            test.populate_callout_script("rc0.sh"); // no versioning
+            test.populate_callout_script("ver-rc1.sh"); // versioning error
+        },
+    );
+}
+
+fn test_undefine_helper<F>(
+    testname: &str,
+    expect: Expect,
+    uuid: &str,
+    parent: Option<String>,
+    force: bool,
+    setupfn: F,
+) where
+    F: Fn(&TestEnvironment),
+{
+    let test = TestEnvironment::new("undefine", testname);
+    setupfn(&test);
+    let uuid = Uuid::parse_str(uuid).unwrap();
+
+    let result = crate::undefine_command(&test, uuid, parent.clone(), force);
+
+    if test.assert_result(result, expect, None).is_err() {
+        return;
+    }
+
+    let devs = crate::defined_devices(&test, Some(&uuid), parent.as_ref())
+        .expect("failed to query defined devices");
+    assert!(devs.is_empty());
+}
+
+#[test]
+fn test_undefine() {
+    init();
+
+    const UUID: &str = "976d8cc2-4bfc-43b9-b9f9-f4af2de91ab9";
+    const PARENT: &str = "0000:00:03.0";
+    const PARENT2: &str = "0000:00:02.0";
+
+    test_undefine_helper(
+        "single",
+        Expect::Pass,
+        UUID,
+        Some(PARENT.to_string()),
+        false,
+        |test| {
+            test.populate_defined_device(UUID, PARENT, "defined.json");
+        },
+    );
+    // if multiple devices with the same uuid exists, the one with the matching parent should
+    // be undefined
+    test_undefine_helper(
+        "multiple-parent",
+        Expect::Pass,
+        UUID,
+        Some(PARENT.to_string()),
+        false,
+        |test| {
+            test.populate_defined_device(UUID, PARENT, "defined.json");
+            test.populate_defined_device(UUID, PARENT2, "defined.json");
+        },
+    );
+    // if multiple devices with the same uuid exists and no parent is specified, they should
+    // all be undefined
+    test_undefine_helper(
+        "multiple-noparent",
+        Expect::Pass,
+        UUID,
+        None,
+        false,
+        |test| {
+            test.populate_defined_device(UUID, PARENT, "defined.json");
+            test.populate_defined_device(UUID, PARENT2, "defined.json");
+        },
+    );
+    test_undefine_helper(
+        "nonexistent",
+        Expect::Fail(None),
+        UUID,
+        Some(PARENT.to_string()),
+        false,
+        |_| {},
+    );
+
+    // callout script always returns with RC=0
+    test_undefine_helper(
+        "single-callout-all-pass",
+        Expect::Pass,
+        UUID,
+        Some(PARENT.to_string()),
+        false,
+        |test| {
+            test.populate_defined_device(UUID, PARENT, "defined.json");
+            test.populate_callout_script("rc0.sh");
+        },
+    );
+    // callout script rejects in pre event undefine with RC=1
+    test_undefine_helper(
+        "single-callout-pre-fail",
+        Expect::Fail(None),
+        UUID,
+        Some(PARENT.to_string()),
+        false,
+        |test| {
+            test.populate_defined_device(UUID, PARENT, "defined.json");
+            test.populate_callout_script("rc1.sh");
+        },
+    );
+    // force command even with callout script failure
+    test_undefine_helper(
+        "single-callout-pre-fail-force",
+        Expect::Pass,
+        UUID,
+        Some(PARENT.to_string()),
+        true,
+        |test| {
+            test.populate_defined_device(UUID, PARENT, "defined.json");
+            test.populate_callout_script("rc1.sh");
+        },
+    );
+
+    // test define with versioning callouts
+    // uuid=11111111-1111-0000-0000-000000000000 has a supported version
+    const UUID_VER: &str = "11111111-1111-0000-0000-000000000000";
+    test_undefine_helper(
+        "undefine-single-with-version-callout-all-pass",
+        Expect::Pass,
+        UUID_VER,
+        Some(PARENT.to_string()),
+        false,
+        |test| {
+            test.populate_defined_device(UUID_VER, PARENT, "defined.json");
+            test.populate_callout_script("ver-rc0.sh"); // versioning
+        },
+    );
+    test_undefine_helper(
+        "undefine-single-with-version-callout-all-fail",
+        Expect::Fail(None),
+        UUID_VER,
+        Some(PARENT.to_string()),
+        false,
+        |test| {
+            test.populate_defined_device(UUID_VER, PARENT, "defined.json");
+            test.populate_callout_script("ver-rc1.sh"); // versioning error
+        },
+    );
+    test_undefine_helper(
+        "define-with-version-callout-multiple-with-version-pass",
+        Expect::Pass,
+        UUID_VER,
+        Some(PARENT.to_string()),
+        false,
+        |test| {
+            test.populate_defined_device(UUID_VER, PARENT, "defined.json");
+            test.populate_callout_script("rc0.sh"); // no versioning
+            test.populate_callout_script("ver-rc0.sh"); // versioning
+        },
+    );
+    test_undefine_helper(
+        "define-with-version-callout-multiple-with-version-pass2",
+        Expect::Pass,
+        UUID_VER,
+        Some(PARENT.to_string()),
+        false,
+        |test| {
+            test.populate_defined_device(UUID_VER, PARENT, "defined.json");
+            test.populate_callout_script("rc1.sh"); // no versioning error
+            test.populate_callout_script("ver-rc0.sh"); // versioning
+        },
+    );
+    test_undefine_helper(
+        "define-with-version-callout-multiple-with-version-fail",
+        Expect::Fail(None),
+        UUID_VER,
+        Some(PARENT.to_string()),
+        false,
+        |test| {
+            test.populate_defined_device(UUID_VER, PARENT, "defined.json");
+            test.populate_callout_script("rc0.sh"); // no versioning
+            test.populate_callout_script("ver-rc1.sh"); // versioning error
+        },
+    );
+}
---
+++ src/tests/mod.rs
@@ -0,0 +1,333 @@
+use anyhow::{anyhow, Result};
+use log::info;
+use nix::sys::wait::waitpid;
+use nix::unistd::{fork, ForkResult};
+use std::collections::BTreeMap;
+use std::env;
+use std::fs;
+use std::path::{Path, PathBuf};
+use std::sync::Mutex;
+use tempfile::Builder;
+use tempfile::TempDir;
+use uuid::Uuid;
+
+use crate::callouts::*;
+use crate::environment::Environment;
+use crate::logger::logger;
+use crate::mdev::MDev;
+
+// additional tests
+mod callouts;
+mod define;
+mod list;
+mod modify;
+mod startstop;
+mod types;
+
+const TEST_DATA_DIR: &str = "testdata";
+
+fn init() {
+    let _ = logger().is_test(true).try_init();
+}
+
+#[derive(PartialEq, Clone, Copy)]
+enum Expect<'a> {
+    Pass,
+    Fail(Option<&'a str>),
+}
+
+#[derive(Debug)]
+struct TestEnvironment {
+    datapath: PathBuf,
+    scratch: TempDir,
+    name: String,
+    case: String,
+    callout_scripts: Mutex<CalloutScriptCache>,
+}
+
+impl Environment for TestEnvironment {
+    fn root(&self) -> &Path {
+        self.scratch.path()
+    }
+
+    fn find_script(&self, dev: &MDev) -> Option<CalloutScriptInfo> {
+        return self
+            .callout_scripts
+            .lock()
+            .unwrap()
+            .find_versioned_script(dev);
+    }
+}
+
+impl TestEnvironment {
+    pub fn new(testname: &str, testcase: &str) -> TestEnvironment {
+        let path: PathBuf = [TEST_DATA_DIR, testname].iter().collect();
+        let scratchdir = Builder::new().prefix("mdevctl-test").tempdir().unwrap();
+        let test = TestEnvironment {
+            datapath: path,
+            scratch: scratchdir,
+            name: testname.to_owned(),
+            case: testcase.to_owned(),
+            callout_scripts: Mutex::new(CalloutScriptCache::new()),
+        };
+        // populate the basic directories in the environment
+        fs::create_dir_all(test.mdev_base()).expect("Unable to create mdev_base");
+        fs::create_dir_all(test.config_base()).expect("Unable to create config_base");
+        fs::create_dir_all(test.parent_base()).expect("Unable to create parent_base");
+        for dir in test.callout_dirs() {
+            fs::create_dir_all(&dir)
+                .expect(format!("Unable to create callout_dir {:?}", &dir).as_str());
+        }
+        for dir in test.notification_dirs() {
+            fs::create_dir_all(&dir)
+                .expect(format!("Unable to create notification_dir '{:?}'", &dir).as_str());
+        }
+        info!("---- Running test '{}/{}' ----", testname, testcase);
+        test
+    }
+
+    // set up a few files in the test environment to simulate an defined mediated device
+    fn populate_defined_device(&self, uuid: &str, parent: &str, filename: &str) {
+        let jsonfile = self.datapath.join(filename);
+        let parentdir = self.config_base().join(parent);
+        fs::create_dir_all(&parentdir).expect("Unable to setup parent dir");
+        let deffile = parentdir.join(uuid);
+        assert!(jsonfile.exists());
+        assert!(!deffile.exists());
+        fs::copy(jsonfile, deffile).expect("Unable to copy device def");
+    }
+
+    // set up a few files in the test environment to simulate an active mediated device
+    fn populate_active_device(&self, uuid: &str, parent: &str, mdev_type: &str) {
+        use std::os::unix::fs::symlink;
+
+        let (parentdir, parenttypedir) =
+            self.populate_parent_device(parent, mdev_type, 1, "", "", None);
+
+        let parentdevdir = parentdir.join(uuid);
+        fs::create_dir_all(&parentdevdir).expect("Unable to setup parent device dir");
+
+        let devdir = self.mdev_base().join(uuid);
+        fs::create_dir_all(&devdir.parent().unwrap()).expect("Unable to setup mdev dir");
+        symlink(&parentdevdir, &devdir).expect("Unable to setup mdev dir");
+
+        let typefile = devdir.join("mdev_type");
+        symlink(&parenttypedir, &typefile).expect("Unable to setup mdev type");
+    }
+
+    // set up a script in the test environment to simulate a callout
+    fn populate_callout_script(&self, filename: &str) {
+        self.populate_callout_script_full(filename, None, true)
+    }
+
+    // set up a script in the test environment to simulate a callout
+    fn populate_callout_script_full(
+        &self,
+        filename: &str,
+        destname: Option<&str>,
+        default_dir: bool,
+    ) {
+        let calloutscriptdir: PathBuf = [TEST_DATA_DIR, "callouts"].iter().collect();
+        let calloutscript = calloutscriptdir.join(filename);
+        let dest = match default_dir {
+            true => self.callout_dir(),
+            false => self.old_callout_dir(),
+        }
+        .join(destname.unwrap_or(filename));
+        assert!(calloutscript.exists());
+
+        /* Because the test suite is multi-threaded, we end up having the same flaky failures
+         * described in this bug: https://github.com/golang/go/issues/22315. When we copy the
+         * callout script into the test environment, another thread might be in the middle of
+         * forking. This fork would then inherit the open writable file descriptor from the parent.
+         * If that child process file descriptor stays open until we try to execute this callout
+         * script, the script will fail to run and we'll get an ETXTBSY error from the OS. In order
+         * to avoid this, we need to avoid the possibility of having any open writable file
+         * descriptors to executable files in the parent process that could be inherited by forks
+         * in other threads. Copying executable files in a child process avoid this. */
+        match unsafe { fork() }.expect("failed to fork") {
+            ForkResult::Parent { child } => {
+                waitpid(child, None).expect("Failed to wait for child");
+            }
+            ForkResult::Child => {
+                fs::copy(calloutscript, &dest).expect("Unable to copy callout script");
+                unsafe {
+                    libc::_exit(0);
+                }
+            }
+        }
+    }
+
+    // set up a few files in the test environment to simulate a parent device that supports
+    // mediated devices
+    fn populate_parent_device(
+        &self,
+        parent: &str,
+        supported_type: &str,
+        instances: i32,
+        device_api: &str,
+        name: &str,
+        description: Option<&str>,
+    ) -> (PathBuf, PathBuf) {
+        let parentdir = self.parent_base().join(parent);
+        let parenttypedir = parentdir.join("mdev_supported_types").join(supported_type);
+        fs::create_dir_all(&parenttypedir).expect("Unable to setup mdev parent type");
+
+        let instancefile = parenttypedir.join("available_instances");
+        fs::write(instancefile, format!("{}", instances))
+            .expect("Unable to write available_instances");
+
+        let apifile = parenttypedir.join("device_api");
+        fs::write(apifile, format!("{}", device_api)).expect("Unable to write device_api");
+
+        let namefile = parenttypedir.join("name");
+        fs::write(namefile, format!("{}", name)).expect("Unable to write name");
+
+        if let Some(desc) = description {
+            let descfile = parenttypedir.join("description");
+            fs::write(descfile, format!("{}", desc)).expect("Unable to write description");
+        }
+
+        (parentdir, parenttypedir)
+    }
+
+    fn compare_to_file(&self, filename: &str, actual: &str) {
+        let path = self.datapath.join(filename);
+        let flag = get_flag(REGEN_FLAG);
+        if flag {
+            regen(&path, actual).expect("Failed to regenerate expected output");
+        }
+        let expected = fs::read_to_string(path).unwrap_or_else(|e| {
+            if e.kind() == std::io::ErrorKind::NotFound {
+                println!(
+                    "File {:?} not found, run tests with {}=1 to automatically \
+                         generate expected output",
+                    filename, REGEN_FLAG
+                );
+            }
+            Default::default()
+        });
+
+        assert_eq!(expected, actual);
+    }
+
+    fn load_from_json<'a>(&'a self, uuid: &str, parent: &str, filename: &str) -> Result<MDev<'a>> {
+        let path = self.datapath.join(filename);
+        let uuid = Uuid::parse_str(uuid);
+        assert!(uuid.is_ok());
+        let uuid = uuid.unwrap();
+        let mut dev = MDev::new(self, uuid);
+
+        let jsonstr = fs::read_to_string(path)?;
+        let jsonval: serde_json::Value = serde_json::from_str(&jsonstr)?;
+        dev.load_from_json(parent.to_string(), &jsonval)?;
+
+        Ok(dev)
+    }
+
+    fn assert_result<T: std::fmt::Debug>(
+        &self,
+        res: Result<T>,
+        expect: Expect,
+        msg: Option<&str>,
+    ) -> Result<T> {
+        let mut testname = format!("{}/{}", self.name, self.case);
+        if let Some(msg) = msg {
+            testname = format!("{}/{}", testname, msg);
+        }
+        match expect {
+            Expect::Fail(msg) => {
+                let e = res.expect_err(format!("Expected {} to fail", testname).as_str());
+                if let Some(msg) = msg {
+                    assert_eq!(msg, e.to_string());
+                }
+                Err(anyhow!(e))
+            }
+            Expect::Pass => Ok(res.expect(format!("Expected {} to pass", testname).as_str())),
+        }
+    }
+}
+
+fn get_flag(varname: &str) -> bool {
+    match env::var(varname) {
+        Err(_) => {
+            return false;
+        }
+        Ok(s) => match s.trim().parse::<i32>() {
+            Err(_) => return false,
+            Ok(n) => return n > 0,
+        },
+    }
+}
+
+fn regen(filename: &PathBuf, data: &str) -> Result<()> {
+    let parentdir = filename.parent().unwrap();
+    fs::create_dir_all(parentdir)?;
+
+    fs::write(filename, data.as_bytes())
+        .and_then(|_| {
+            println!("Regenerated expected data file {:?}", filename);
+            Ok(())
+        })
+        .map_err(|err| err.into())
+}
+
+const REGEN_FLAG: &str = "MDEVCTL_TEST_REGENERATE_OUTPUT";
+
+fn test_load_json_helper(uuid: &str, parent: &str, expect: Expect) {
+    let test = TestEnvironment::new("load-json", uuid);
+
+    let res = test.load_from_json(uuid, parent, &format!("{}.in", uuid));
+    if let Ok(dev) = test.assert_result(res, expect, None) {
+        let jsonval = dev.to_json(false).unwrap();
+        let jsonstr = serde_json::to_string_pretty(&jsonval).unwrap();
+
+        test.compare_to_file(&format!("{}.out", uuid), &jsonstr);
+        assert_eq!(uuid, dev.uuid.hyphenated().to_string());
+        assert_eq!(Some(parent.to_string()), dev.parent);
+    }
+}
+
+#[test]
+fn test_load_json() {
+    init();
+
+    test_load_json_helper(
+        "c07ab7b2-8aa2-427a-91c6-ffc949bb77f9",
+        "0000:00:02.0",
+        Expect::Pass,
+    );
+    test_load_json_helper(
+        "783e6dbb-ea0e-411f-94e2-717eaad438bf",
+        "0001:00:03.1",
+        Expect::Pass,
+    );
+    test_load_json_helper(
+        "5269fe7a-18d1-48ad-88e1-3fda4176f536",
+        "0000:00:03.0",
+        Expect::Pass,
+    );
+    test_load_json_helper(
+        "5269fe7a-18d1-48ad-88e1-3fda4176f536",
+        "0000:00:03.0",
+        Expect::Pass,
+    );
+    // json file has malformed attributes - an array of one object with multiple fields
+    test_load_json_helper(
+        "b6f7e33f-ea28-4f9d-8c42-797ff0ec2888",
+        "0000:00:03.0",
+        Expect::Fail(None),
+    );
+    // json file has malformed attributes - an array of strings
+    test_load_json_helper(
+        "fe7a39db-973b-47b4-9b77-1d7b97267d59",
+        "0000:00:03.0",
+        Expect::Fail(None),
+    );
+    // json file has malformed attributes - no array
+    test_load_json_helper(
+        "37ccb149-a0ce-49e3-8391-a952ef07bdc2",
+        "0000:00:03.0",
+        Expect::Fail(None),
+    );
+}
